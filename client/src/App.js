982f6fd1a9413dab03f0d367bceff368266910ba
Btissam Yaqine <btissamyaqine123@gmail.com>
2022-06-29 16:11:19 +0100

    fixed the cors isseu

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
-import Auth from './components/Auth/Auth'
+import Auth from './components/Auth/Auth';
+
 
 
 
